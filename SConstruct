343c4b1e15bb7c66c84d7e9d15f2d23d9e0d7a85
maureeungaro <maurizio.ungaro@cnu.edu>
2015-07-27 16:20:24 -0400

    added main Sconstruct and changelog

---
+++ SConstruct
@@ -0,0 +1,190 @@
+from init_env import init_environment
+
+# adding ccdb as temporary dependency
+# will be removed once the hit process routines are plugins
+env = init_environment("qt5 geant4 clhep evio xercesc ccdb")
+
+# env.Replace(CXX = "/apps/gcc/4.7.2/bin/g++")
+# env.Replace(CXX = "/usr/bin/clang++")
+#env.Append(CXXFLAGS='-Wno-shorten-64-to-32')
+#env.Append(CXXFLAGS='-Wno-sign-conversion')
+
+# added because clhep is still behind clang (5/2015)
+if env['PLATFORM'] == 'darwin':
+	env.Append(CXXFLAGS='-Wno-absolute-value')
+
+# Materials
+env.Append(CPPPATH = 'materials')
+materials_sources = Split("""
+	materials/material_factory.cc
+	materials/cpp_materials.cc
+	materials/mysql_materials.cc
+	materials/text_materials.cc
+	materials/gdml_materials.cc""")
+env.Library(source = materials_sources, target = "lib/materials")
+
+# Mirrors
+env.Append(CPPPATH = 'mirrors')
+mirrors_sources = Split("""
+	mirrors/mirrors_factory.cc
+	mirrors/mysql_mirrors.cc
+	mirrors/text_mirrors.cc""")
+env.Library(source = mirrors_sources, target = "lib/mirrors")
+
+
+# Parameters
+env.Append(CPPPATH = 'parameters')
+parameters_sources = Split("""
+	parameters/parameter_factory.cc
+	parameters/mysql_parameters.cc
+	parameters/text_parameters.cc""")
+env.Library(source = parameters_sources, target = "lib/parameters")
+
+
+# Utilities
+env.Append(CPPPATH = 'utilities')
+util_sources = Split("""
+	utilities/string_utilities.cc
+	utilities/utils.cc
+	utilities/lStdHep.cc
+	utilities/lXDR.cc
+	utilities/options.cc""")
+env.Library(source = util_sources, target = "lib/utilities")
+
+
+# Detector
+env.Append(CPPPATH = 'detector')
+det_sources = Split("""
+	detector/detector.cc
+	detector/detector_factory.cc
+	detector/mysql_det_factory.cc
+	detector/gdml_det_factory.cc
+	detector/clara_det_factory.cc
+	detector/gdml_solids.cc
+	detector/text_det_factory.cc""")
+env.Library(source = det_sources, target = "lib/detector")
+
+
+# Sensitivity
+env.Append(CPPPATH = 'sensitivity')
+sensi_sources = Split("""
+	sensitivity/sensitiveDetector.cc
+	sensitivity/identifier.cc
+	sensitivity/Hit.cc
+	sensitivity/HitProcess.cc
+	sensitivity/sensitiveID.cc""")
+env.Library(source = sensi_sources, target = "lib/sensitivity")
+
+
+# Physics
+env.Append(CPPPATH = 'physics')
+phys_sources = Split("""
+	physics/PhysicsList.cc
+	physics/PhysicsListMessenger.cc""")
+env.Library(source = phys_sources, target = "lib/physics")
+
+# Fields
+env.Append(CPPPATH = 'fields')
+field_sources = Split("""
+	fields/field.cc
+	fields/fieldFactory.cc
+	fields/asciiField.cc
+	fields/mappedField.cc
+	fields/multipoleField.cc 
+	fields/symmetries/dipole.cc
+	fields/symmetries/cylindrical.cc
+	fields/symmetries/phi-segmented.cc""")
+env.Library(source = field_sources, target = "lib/fields")
+
+
+# Hit Processes
+env.Append(CPPPATH = ['hitprocess', 'hitprocess/clas12', 'hitprocess/clas12/svt', 'hitprocess/clas12/micromegas'])
+env.Append(CPPPATH = ['hitprocess/Aprime', 'hitprocess/GlueX', 'hitprocess/solid'])
+hitp_sources = Split("""
+	hitprocess/HitProcess_MapRegister.cc
+	hitprocess/flux_hitprocess.cc
+	hitprocess/clas12/micromegas/FMT_hitprocess.cc
+	hitprocess/clas12/micromegas/fmt_strip.cc	
+	hitprocess/clas12/micromegas/BMT_hitprocess.cc
+	hitprocess/clas12/micromegas/bmt_strip.cc
+	hitprocess/clas12/micromegas/ftm_hitprocess.cc
+	hitprocess/clas12/micromegas/ftm_strip.cc
+	hitprocess/clas12/svt/bst_hitprocess.cc
+	hitprocess/clas12/svt/bst_strip.cc
+	hitprocess/clas12/ctof_hitprocess.cc
+	hitprocess/clas12/cnd_hitprocess.cc
+	hitprocess/clas12/dc_hitprocess.cc
+	hitprocess/clas12/ec_hitprocess.cc
+	hitprocess/clas12/ecs_hitprocess.cc
+	hitprocess/clas12/ftof_hitprocess.cc
+	hitprocess/clas12/ft_cal_hitprocess.cc
+	hitprocess/clas12/ft_hodo_hitprocess.cc
+	hitprocess/clas12/htcc_hitprocess.cc
+	hitprocess/clas12/pcal_hitprocess.cc
+	hitprocess/clas12/rich_hitprocess.cc
+	hitprocess/bdx/cormo_hitprocess.cc
+	hitprocess/bdx/veto_hitprocess.cc
+	hitprocess/HPS/ECAL_hitprocess.cc
+	hitprocess/HPS/SVT_hitprocess.cc
+	hitprocess/HPS/muon_hodo_hitprocess.cc""")
+env.Library(source = hitp_sources, target = "lib/hitprocess")
+
+
+# Output
+env.Append(CPPPATH = 'output')
+output_sources = Split("""
+	output/outputFactory.cc
+	output/evio_output.cc
+	output/txt_output.cc
+	output/gbank.cc""")
+env.Library(source = output_sources, target = "lib/output")
+
+# GUI
+env.Append(CPPPATH = 'gui/src')
+gui_sources = Split("""
+	gui/src/gemc_MainGui.cc
+	gui/src/detector_editor.cc
+	gui/src/runControl/run_control.cc
+	gui/src/runControl/primaryTab.cc
+	gui/src/runControl/momControls.cc
+	gui/src/runControl/vtxControls.cc
+	gui/src/camera_control.cc
+	gui/src/detector_tree.cc
+	gui/src/infos.cc
+	gui/src/g4dialog.cc
+	gui/src/gsignal.cc
+	gui/src/physicsListGui.cc""")
+env.Library(source = gui_sources, target = "lib/gui")
+
+
+# gemc
+env.Append(CPPPATH = 'src')
+gemc_sources = Split("""
+	gemc.cc
+	src/dmesg_init.cc
+	src/run_conditions.cc
+	src/gemc_options.cc
+	src/MDetectorConstruction.cc
+	src/MDetectorMessenger.cc
+	src/MEventAction.cc
+	src/MPrimaryGeneratorAction.cc
+	src/MSteppingAction.cc""")
+
+env.Append(LIBPATH = ['lib'])
+env.Prepend(LIBS =  ['materials', 'mirrors', 'parameters', 'utilities', 'detector', 'sensitivity', 'physics', 'fields', 'hitprocess', 'output', 'gui'])
+env.Program(source = gemc_sources, target = "gemc")
+
+
+if env['LIBRARY'] == "static":
+	env.Library(source = gemc_sources, target = "gemc")
+
+
+if env['LIBRARY'] == "shared":
+	env.SharedLibrary(source = gemc_sources, target = "gemc")
+
+
+
+
+
+
+
---
+++ gemc.cc
@@ -0,0 +1,410 @@
+/// \mainpage
+/// \htmlonly <center><img src="gemc_logo.gif" width="130"></center>\endhtmlonly
+/// \section overview Overview
+/// gemc (<b>GE</b>ant4 <b>M</b>onte<b>C</b>arlo) GEMC is a C++ framework
+/// based on <a href="http://geant4.web.cern.ch/geant4/"> Geant4 </a>
+/// Libraries to simulate the passage of particles through matter.\n
+/// The simulation parameters are external to the software:
+/// Geometry, Materials, Fields, Banks definitions are stored in
+/// external databases in various format and are loaded at run
+/// time using factory methods.\n
+/// \section databases Databases
+/// gemc currently supports <i> mysql, gdml, TEXT </i> to build the detector systems: \n
+/// - Geometry.
+/// - Sensitive Detectors.
+/// - Hit Process.
+/// - Banks Format.
+/// - Materials.
+/// \section platforms Platforms Supported:
+/// - <i> Windows 7 to come October 2013 </i>
+/// - Linux (32, 64)
+/// - Mac OS X
+/// \section docs Documentation:
+/// - <a href="http://gemc.jlab.org">  gemc website </a>
+/// \section Dependencies:
+/// - geant4 (simulation libraries)
+/// - clhep  (random generators, physics vectors, geometry and linear algebra libraries)
+/// - qt4 (graphic libraries)
+/// - mysql
+/// - scons (build system)
+/// - xercesc
+/// \image html gemc_logo.gif
+/// \n\n
+/// \author \n &copy; Maurizio Ungaro
+/// \author e-mail: ungaro@jlab.org\n\n\n
+/// \file gemc.cc
+/// Defines the gemc main( int argc, char **argv )
+/// \author \n &copy; Maurizio Ungaro
+/// \author e-mail: ungaro@jlab.org\n\n\n
+
+const char *GEMC_VERSION = "gemc 2.3";
+
+// G4 headers
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "G4UIterminal.hh"
+#include "G4VisExecutive.hh"
+#include "G4VModularPhysicsList.hh"
+#include "G4PropagatorInField.hh"
+#include "G4TransportationManager.hh"
+#include "G4UIQt.hh"
+#include "G4Qt.hh"
+
+// Qt headers
+#include <QApplication>
+#include <QSplashScreen>
+
+// gemc headers
+#include "HitProcess_MapRegister.h"
+#include "detector_factory.h"
+#include "gemc_MainGui.h"
+#include "gbank.h"
+#include "MDetectorConstruction.h"
+#include "MEventAction.h"
+#include "outputFactory.h"
+#include "HitProcess.h"
+#include "PhysicsList.h"
+#include "MPrimaryGeneratorAction.h"
+#include "MSteppingAction.h"
+#include "options.h"
+#include "dmesg_init.h"
+#include "run_conditions.h"
+#include "fieldFactory.h"
+#include "material_factory.h"
+#include "mirrors_factory.h"
+#include "parameter_factory.h"
+#include "string_utilities.h"
+#include "utils.h"
+
+// c++ headers
+#include <unistd.h>  // needed for get_pid
+
+/////////////////////////
+/// <b> Main Program </b>
+/////////////////////////
+///  -# Sets the goptions\n
+///  -# Starts QT application if USE_GUI=1
+///  -# Starts the CLHEP random engine
+///  -# Instantiates the Geant4 Run Manager
+///  -# Builds detector map object from database
+///  -# Builds Processes Routines Map
+///  -# Builds Materials Map
+///  -# Builds G4 Physical Volumes
+///  -# Initialize Physics List
+///  -# Initialize Generator
+///  -# Initialize Event Action
+///  -# Initialize G4Qt User Interface if USE_GUI>0
+///  -# Initialize Visualization Manager if USE_GUI>0
+
+
+// get_pid is useful only on the farm to set the seed
+// can set to zero in Windows environment
+// ideally we'd want __get_pid();
+#ifdef _MSC_VER
+#include <stdio.h>
+#include <process.h>
+	//int get_pid(){return __get_pid();}
+	int get_pid(){return 0;}
+#endif
+
+// distinguishing between graphical and batch mode
+QCoreApplication* createApplication(int &argc, char *argv[], double use_gui)
+{
+	if(!use_gui)
+			return new QCoreApplication(argc, argv);
+	return new QApplication(argc, argv);
+}
+
+
+int main( int argc, char **argv )
+{
+	clock_t startTime = clock();
+	cout << endl;
+	goptions gemcOpt;
+	gemcOpt.setGoptions();
+	gemcOpt.setOptMap(argc, argv);
+	
+	double use_gui   = gemcOpt.optMap["USE_GUI"].arg;
+
+	cout << endl << "  > Initializing GEant4 MonteCarlo:  " << GEMC_VERSION << endl << endl;
+	
+	QScopedPointer<QCoreApplication> app(createApplication(argc, argv, use_gui));
+
+	// Initializing gemc splash class
+	// This class will log initialization messages
+	// This class will show a splashscreen if use_gui is non zero
+	// The screen log verbosity is controlled by LOG_VERBOSITY
+	gui_splash gemc_splash(gemcOpt);
+	gemc_splash.message(" Initializing GEant4 MonteCarlo version " + string(GEMC_VERSION));
+	
+	
+	// random seed initialization
+	CLHEP::HepRandom::setTheEngine(new CLHEP::MTwistEngine);
+	G4int seed;
+	
+	if(gemcOpt.optMap["RANDOM"].args=="TIME")
+	{
+		gemc_splash.message(" Initializing CLHEP Random Engine from local time " \
+							+ stringify((double) time(NULL)) \
+							+ ", cpu clock "        \
+							+ stringify((double) clock())    \
+							+ " and process id "    \
+							+ stringify(getpid()) + ".");
+		seed = (G4int) ( (double) time(NULL)- (double) clock()-getpid() );
+	}
+	else
+	{
+		seed = atoi(gemcOpt.optMap["RANDOM"].args.c_str());
+		gemc_splash.message(" Initializing CLHEP Random Engine from user defined seed.");
+	}
+	
+	CLHEP::HepRandom::setTheSeed(seed);
+	gemc_splash.message(" Seed initialized to: " + stringify(seed));
+	
+	// Construct the default G4 run manager
+	gemc_splash.message(" Instantiating Run Manager...");
+	G4RunManager *runManager = new G4RunManager;
+	
+	// Initializing run_condition class
+	gemc_splash.message(" Instantiating Run Conditions...");
+	runConditions runConds(gemcOpt);
+	
+	
+	// GEMC Detector Map
+	gemc_splash.message(" Registering Detectors Factories...");
+	// Initializing Detector Factory
+	map<string, detectorFactoryInMap> detectorFactoryMap = registerDetectorFactory();
+	// Building detector with factories
+	map<string, detector> hallMap = buildDetector(detectorFactoryMap, gemcOpt, runConds);
+	
+	
+	// Initialize Materials Map Factory
+	gemc_splash.message(" Initializing Material Factories..." );
+	map<string, materialFactory> materialFactoriesMap = registerMaterialFactories();
+	// Build all materials
+	map<string, G4Material*> mats = buildMaterials(materialFactoriesMap, gemcOpt, runConds);
+	
+	// Initialize Mirrors Map Factory
+	gemc_splash.message(" Initializing Mirrors Factories..." );
+	map<string, mirrorFactory> mirrorFactoriesMap = registerMirrorFactories();
+	// Build all mirrors
+	map<string, mirror*> mirs = buildMirrors(mirrorFactoriesMap, gemcOpt, runConds);
+	
+	
+	// Initialize Parameters Map Factory
+	gemc_splash.message(" Registering Parameters Factories...");
+	map<string, parameterFactoryInMap> parameterFactoriesMap = registerParameterFactories();
+	// All Parameters with factories
+	map<string, double> gParameters = loadAllParameters(parameterFactoriesMap, gemcOpt, runConds);
+	
+	
+	// Process Hit Map
+	gemc_splash.message(" Building gemc Process Hit Factory...");
+	map<string, HitProcess_Factory> hitProcessMap = HitProcess_Map(gemcOpt.optMap["HIT_PROCESS_LIST"].args);
+	
+	///< magnetic Field Map
+	gemc_splash.message(" Creating fields Map...");
+	map<string, fieldFactoryInMap> fieldFactoryMap = registerFieldFactories();
+	map<string, gfield> fieldsMap = loadAllFields(fieldFactoryMap, gemcOpt);
+	
+	// Build the detector
+	gemc_splash.message(" Building Detector Map...");
+	MDetectorConstruction* ExpHall = new MDetectorConstruction(gemcOpt);
+	ExpHall->hallMap   = &hallMap;
+	ExpHall->mirs      = &mirs;
+	ExpHall->mats      = &mats;
+	ExpHall->fieldsMap = &fieldsMap;
+	// this is what calls Construct inside MDetectorConstruction
+	runManager->SetUserInitialization(ExpHall);
+	
+	
+	///< Physics List
+	string phys_list = gemcOpt.optMap["PHYSICS"].args  ;
+	gemc_splash.message(" Initializing Physics List " + phys_list + "...");
+	runManager->SetUserInitialization(new PhysicsList(gemcOpt));
+
+	// Setting Max step for all the simulation. This is historically needed to limit
+	// the step in magnetic field in vacuum
+	double max_step = gemcOpt.optMap["MAX_FIELD_STEP"].arg;
+	if(max_step != 0)
+		G4TransportationManager::GetTransportationManager()->GetPropagatorInField()->SetLargestAcceptableStep(max_step);
+	
+	// Generator
+	gemc_splash.message(" Initializing Primary Generator Action...");
+	MPrimaryGeneratorAction* gen_action = new MPrimaryGeneratorAction(&gemcOpt);
+	runManager->SetUserAction(gen_action);
+	
+	
+	// Event Action
+	gemc_splash.message(" Initializing Event Action...");
+	MEventAction* event_action = new MEventAction(gemcOpt, gParameters);
+	event_action->SetEvtNumber((int) gemcOpt.optMap["EVN"].arg);     ///< Sets event number from OPTION
+	runManager->SetUserAction(event_action);
+	
+	// Stepping Action
+	gemc_splash.message(" Initializing Stepping Action...");
+	MSteppingAction* SteppingAction = new MSteppingAction(gemcOpt);
+	runManager->SetUserAction(SteppingAction);
+	
+	///< User Interface manager
+	gemc_splash.message(" Initializing User Interface...");
+	G4UIsession *session = NULL;
+	
+	///< Vis Manager
+	G4VisManager *visManager = NULL;
+	if(use_gui)
+	{
+		visManager = new G4VisExecutive;
+		visManager->Initialize();
+	}
+
+	if(use_gui)
+		session = new G4UIQt(1, argv);
+
+	
+	
+	// Output File: registering output type, output process factory,
+	// sensitive detectors into Event Action
+	gemc_splash.message(" Initializing Output Action...");
+	outputContainer outContainer(gemcOpt);
+	map<string, outputFactoryInMap> outputFactoryMap = registerOutputFactories();
+
+	// Initialize G4 kernel
+	gemc_splash.message(" Initializing Run Manager...\n");
+	// physical volumes, sensitive detectors are built here
+	runManager->Initialize();
+
+	
+	
+	// registering activated field in the option so they're written out
+	if(ExpHall->activeFields.size())
+	gemcOpt.optMap["ACTIVEFIELDS"].args = "";
+	
+	for(set<string>::iterator fit = ExpHall->activeFields.begin(); fit != ExpHall->activeFields.end(); fit++)
+		gemcOpt.optMap["ACTIVEFIELDS"].args = gemcOpt.optMap["ACTIVEFIELDS"].args + *fit + " ";
+	
+	
+	// Creating the sim_condition map to save to the output
+	gemc_splash.message(" Writing simulation parameters in the output...");
+	
+	// filling gcard option content
+	map<string, string> sim_condition = gemcOpt.getOptMap();
+	// adding detectors conditions to sim_condition
+	mergeMaps(sim_condition, runConds.getDetectorConditionsMap());
+	// adding parameters value to sim_condition
+	mergeMaps(sim_condition, getParametersMap(gParameters));
+
+	
+	// Bank Map, derived from sensitive detector map
+	gemc_splash.message(" Creating gemc Banks Map...");
+	map<string, gBank> banksMap = read_banks(gemcOpt, runConds.get_systems());
+		
+	// Getting UI manager, restoring G4Out to cout
+	G4UImanager* UImanager = G4UImanager::GetUIpointer();
+	UImanager->SetCoutDestination(NULL);
+
+
+	// saving simulation condition in the output file
+	if(outContainer.outType != "no")
+	{
+		outputFactory *processOutputFactory  = getOutputFactory(&outputFactoryMap, outContainer.outType);
+		processOutputFactory->recordSimConditions(&outContainer, sim_condition);
+		// then deleting process output pointer, not needed anymore
+		delete processOutputFactory;
+	}
+	
+
+	event_action->outContainer     = &outContainer;
+	event_action->outputFactoryMap = &outputFactoryMap;
+	event_action->hitProcessMap    = &hitProcessMap;
+	event_action->SeDe_Map         = ExpHall->SeDe_Map;
+	event_action->banksMap         = &banksMap;
+	event_action->gen_action       = gen_action;
+ 	
+	///< passing output process factory to sensitive detectors
+	map<string, sensitiveDetector*>::iterator it;
+	for(it = ExpHall->SeDe_Map.begin(); it != ExpHall->SeDe_Map.end(); it++)
+		it->second->hitProcessMap = &hitProcessMap;
+	
+	
+
+	
+	gemc_splash.message(" Executing initial directives...\n");
+	vector<string> init_commands = init_dmesg(gemcOpt);
+	for(unsigned int i=0; i<init_commands.size(); i++)
+		UImanager->ApplyCommand(init_commands[i].c_str());
+	string exec_macro = "/control/execute " + gemcOpt.optMap["EXEC_MACRO"].args;
+	
+	clock_t start_events;
+
+	if(use_gui)
+	{
+		gemc_splash.message("Starting GUI...");
+		qApp->processEvents();
+		
+		gemcMainWidget gemcW(&gemcOpt, runManager, ExpHall->SeDe_Map, &hallMap, mats);
+		gemcW.setWindowTitle(GEMC_VERSION);
+		vector<string> wpos =  get_info(gemcOpt.optMap["GUIPOS"].args);
+
+		gemcW.move(get_number(wpos[0]), get_number(wpos[1]));
+		gemcW.show();
+		
+		// splash can finish once gemcW is up
+		gemc_splash.splash->finish(&gemcW);
+		
+		gemc_splash.message(" Executing initial visual directives...\n");
+		vector<string> init_vcommands = init_dvmesg(gemcOpt, visManager);
+		for(unsigned int i=0; i<init_vcommands.size(); i++)
+		{
+			gemc_splash.message(" Now executing: " + init_vcommands[i]);
+			
+			UImanager->ApplyCommand(init_vcommands[i].c_str());
+		}
+		
+		if(exec_macro != "/control/execute no") UImanager->ApplyCommand(exec_macro.c_str());
+		if(gemcOpt.optMap["N"].arg>0)
+		{
+			char command[100];
+			sprintf(command, "/run/beamOn %d", (int) gemcOpt.optMap["N"].arg);
+			UImanager->ApplyCommand(command);
+		}
+		
+		start_events = clock();
+		return qApp->exec();
+		// deleting and runManager is now taken care
+		// in the gemc_quit slot
+		delete visManager;
+		if(session != NULL) delete session;
+	}
+	else
+	{
+		
+		if(exec_macro != "/control/execute no") UImanager->ApplyCommand(exec_macro.c_str());
+		start_events = clock();
+		if(gemcOpt.optMap["N"].arg>0)
+		{
+			char command[100];
+			sprintf(command, "/run/beamOn %d", (int) gemcOpt.optMap["N"].arg);
+			UImanager->ApplyCommand(command);
+		}
+	}
+
+	clock_t endTime = clock();
+	clock_t clockAllTaken   = endTime - startTime;
+	clock_t clockEventTaken = endTime - start_events;
+
+	cout << " > Total gemc time: " <<  clockAllTaken / (double) CLOCKS_PER_SEC << " seconds. "
+	     << " Events only time: " << clockEventTaken / (double) CLOCKS_PER_SEC << " seconds. " << endl;
+
+	
+	delete runManager;
+	return 1;
+}
+
+
+
+
+
+
+
+
