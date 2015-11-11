257855870a3af6303f1e43e402611d2be0f71ef9
Veronique Ziegler <ziegler@jlab.org>
2015-11-10 19:14:16 -0500

    new geom

--- hitprocess/clas12/micromegas/bmt_strip.cc
+++ hitprocess/clas12/micromegas/bmt_strip.cc
@@ -8,335 +8,105 @@ void bmt_strip::fill_infos()
 {
  // all dimensions are in mm
     Pi  = 3.14159265358;
+    SigmaDrift = 0.4;
+    hDrift = 3.0;
+    hStrip2Det = hDrift/2.;
+    ThetaL = toRadians(20.);
 
-    interlayer     = 15.0;
-    Nsector        = 3;
-    hDrift         = 3.0;
-    hStrip2Det     = hDrift/2.;
-    sigma_td_max   = 0.4;
-    theta_L        = Pi*20./180.;
-    w_i            = 25.0;
+    // DEFINING THE Z DETECTORS
+    CRZRADIUS[2]	=	205.8;
+	CRZNSTRIPS[2]	=	768;
+	CRZSPACING[2]	=	0.2;
+	CRZWIDTH[2]		=	0.328;
+	CRZLENGTH[2]	=	444.88;
+	CRZZMIN[2]		=	-421.75;
+	CRZZMAX[2]		=	290.25;
+	CRZOFFSET[2]	=	252.1;
+    CRZEDGE1.resize(3); //3 regions
+    for (int i = 0; i <3 ; ++i)
+    	CRZEDGE1[i].resize(3);
+    CRZEDGE2.resize(3); //3 regions
+	for (int i = 0; i <3 ; ++i)
+		CRZEDGE2[i].resize(3);
 
+    double ZEdge1[] = {toRadians(30.56),toRadians(270.56),toRadians(150.56)};
+    double ZEdge2[] = {toRadians(149.44),toRadians(29.44),toRadians(269.44)};
 
-    interStripZ = 0.20;
-    interStripC = 0.16;
-
- // pitch CR4Z -> CR4 = central tracker region 4
-    pitchZ4 = 0.487; // and interpitch=0.201
- 
- // pitch CR5Z -> CR5 = central tracker region 5
-    pitchZ5 = 0.49; // none existing value. picked a rabdom value compatible with the geometry
- 
- // pitch CR6Z -> CR6 = central tracker region 6
-    pitchZ6 = 0.528; //interStripZ + strip_widthZ(=0.328)
- 
- // pitch CRC --> for the C-detectors, the strips are in bunches of equal pitches
+    // Assume the edge boundaries are the same for all regions until get final geometry
+    for (int i = 0; i <3 ; ++i)	{
+    	for (int j = 0; j <3 ; ++j){
+			CRZEDGE1[i][j] = ZEdge1[j];
+			CRZEDGE2[i][j] = ZEdge2[j];
+		}
+    }
+    CRZXPOS[2]		=	10.547;
+    // DEFINING THE C DETECTORS
+	CRCRADIUS[2]	=	220.8;
+	CRCNSTRIPS[2]	=	1152;
+	CRCLENGTH[2]	=	438.6;
+	CRCSPACING[2]	=	0.16;
+	CRCZMIN[2]		=	-421.75;
+	CRCZMAX[2]		=	290.25;
+	CRCOFFSET[2]	=	252.18;
+    // pitch CRC --> for the C-detectors, the strips are in bunches of equal pitches
     double CR4C_width[13]={0.345,0.28,0.225,0.175,0.17,0.21,0.26,0.31,0.37,0.44,0.515,0.605,0.7};     // width of the corresponding group of strips
     double CR4C_group[13]={32,32,32,32,624,32,32,32,32,32,32,32,896};                              	  // the number of strips with equal pitches
     // For CR5, no existing value. picked a random value compatible with the geometry
-    double CR5C_width[1]={0.253};                                                                     // width of the corresponding group of strips
-    double CR5C_group[1]={1024};                                                                      // the number of strips with equal pitches
-    double CR6C_width[14]={0.38,0.32,0.27,0.23,0.17,0.18,0.22,0.25,0.29,0.33,0.37,0.41,0.46,0.51};    // width of the corresponding group of strips
-    double CR6C_group[14]={32,32,32,32,704,64,32,32,32,32,32,32,32,32};                               // the number of strips with equal pitches
-    int arraySize[] = {sizeof(CR4C_group) / sizeof(CR4C_group[0]),sizeof(CR5C_group) / sizeof(CR5C_group[0]),sizeof(CR6C_group) / sizeof(CR6C_group[0])}; // size of arrays describing the C-detectors for a given MM region
+    double CR5C_width[1]={0.253};                                                                     // the number of strips with equal pitches & width of the corresponding group of strips
+    double width[1]={1024};
+    // For CR6 the numbers are final and should not be changed
+    double CR6C_width[14]={0.38,0.32,0.27,0.23,0.17,0.18,0.22,0.25,0.29,0.33,0.37,0.41,0.46,0.51};    // the number of strips with equal pitches & width of the corresponding group of strips
+    double CR6C_group[14]={32,32,32,32,704,64,32,32,32,32,32,32,32,32};
 
-    // pitch CR4C for first region of macromegas (CRC4)
-    for(int i =0; i<arraySize[0]; i++) {
-    	widthC4.push_back(CR4C_width[i]);
-        pitchC4.push_back(interStripC+CR4C_width[i]);
-        nbunchC4.push_back(CR4C_group[i]);
+    int MxGrpSize = 14; // the max number of entries in CRC_group array
+    CRCGROUP.resize(3); //3 regions
+    CRCWIDTH.resize(3);
+
+    for (int i = 0; i <3 ; ++i)
+    	CRCGROUP[i].resize(MxGrpSize);
+    for(int j =0; j<sizeof(CR4C_group) / sizeof(CR4C_group[0]); j++) { // region index  0 is CR4
+    	CRCGROUP[0][j] = CR4C_group[j];
+    	CRCWIDTH[0][j] = CR4C_width[j];
     }
-    // pitch CR5C for second region of macromegas (CRC5)
-    for(int i =0; i<arraySize[1]; i++) {
-    	widthC5.push_back(CR5C_width[i]);
-        pitchC5.push_back(interStripC+CR5C_width[i]);
-        nbunchC5.push_back(CR5C_group[i]);
+    for(int j =0; j<sizeof(CR5C_group) / sizeof(CR5C_group[0]); j++) { // region index  1 is CR5
+        CRCGROUP[1][j] = CR5C_group[j];
+        CRCWIDTH[1][j] = CR5C_width[j];
     }
-    // pitch CR6C for third region of macromegas (CRC6)
-    for(int i =0; i<arraySize[2]; i++) {
-    	widthC6.push_back(CR6C_width[i]);
-        pitchC6.push_back(interStripC+CR6C_width[i]);
-        nbunchC6.push_back(CR6C_group[i]);
+    for(int j =0; j<sizeof(CR6C_group) / sizeof(CR6C_group[0]); j++) { // region index  2 is CR6
+        CRCGROUP[2][j] = CR6C_group[j];
+        CRCWIDTH[2][j] = CR6C_width[j];
     }
-    // z of the upstream part of the layer
-    Z0.push_back(-127.820); Z0.push_back(-127.820);
-    Z0.push_back(-148.740); Z0.push_back(-148.740);
-    Z0.push_back(-169.710); Z0.push_back(-169.710);
- 
-    // total z length of the layer of the layer
-    DZ.push_back(372.75); DZ.push_back(372.75);
-    DZ.push_back(423.99); DZ.push_back(423.99);
-    DZ.push_back(444.96); DZ.push_back(444.96);
 
-    // radii of layers
-    R.push_back(145.731); R.push_back(R[0]+interlayer);
-    R.push_back(175.731); R.push_back(R[2]+interlayer);
-    R.push_back(205.731); R.push_back(R[4]+interlayer);
+    CRCEDGE1.resize(3); //3 regions
+	for (int i = 0; i <3 ; ++i)
+		CRCEDGE1[i].resize(3);
+	CRCEDGE2.resize(3); //3 regions
+	for (int i = 0; i <3 ; ++i)
+		CRCEDGE2[i].resize(3);
 
-    // Number of strips (depends on radius, dead zones, and pitch!)
-    // layers 0,2,4 are Z detectors, i.e. measure phi (strips in z direction)
-    // layers 1,3,5 are C detectors, i.e. measure z (phi strips at z)
-    Nstrips.push_back(14*64);         // CR4Z
-    Nstrips.push_back(10*64);         // CR4C
-    Nstrips.push_back(11*64);         // CR5Z
-    Nstrips.push_back(16*64);        // CR5C
-    Nstrips.push_back(768);          // CR6Z
-    Nstrips.push_back(1152);         // CR6C
+    double CEdge1[] = {toRadians(30.52),toRadians(270.52),toRadians(150.52)};
+    double CEdge2[] = {toRadians(149.48),toRadians(29.48),toRadians(269.48)};
 
- 
-    // dead angles
-    Inactivtheta.push_back((20/R[0])*(180./Pi));
-    Inactivtheta.push_back((20/R[1])*(180./Pi));
-    Inactivtheta.push_back((20/R[2])*(180./Pi));
-    Inactivtheta.push_back((20/R[3])*(180./Pi));
-    Inactivtheta.push_back((20/R[4])*(180./Pi));
-    Inactivtheta.push_back((20/R[5])*(180./Pi));
- 
-    // dead zones
-    DZ_inLength = 0; //for CRnZ
-    DZ_inWidth  = 0; //for CRnC
-    DZ4_inLength = ((R[1]*(120-Inactivtheta[1])*Pi/180)-(Nstrips[1]*pitchZ4+interStripZ))/2; //for CR4Z (((160.731*(120-7.129)*Pi/180)-(10*64*0.487)+0.201))/2 = 2.377
-    DZ4_inWidth  = (DZ[0]-372.48-interStripC)/2; //for CR4C // (372.75 - 372.48 - 0.16)/2 = 0.055
-    DZ5_inLength = ((R[2]*(120-Inactivtheta[2])*Pi/180)-(Nstrips[2]*pitchZ4+interStripZ))/2; //for CR5Z (((175.731*(120-6.521)*Pi/180)-(11*64*0.49)+0.201))/2 = 1.444
-    DZ5_inWidth  = (DZ[3]-422.912-interStripC)/2; //for CR5C // (423.99 - 422.912 - 0.16)/2 = 0.459
-    DZ6_inLength = ((R[4]*(120-Inactivtheta[4])*Pi/180)-(Nstrips[4]*pitchZ4+interStripZ))/2; //for CR6Z (((205.731*(120-5.57)*Pi/180)-(12*64*0.526)+0.201))/2 = 1.779
-    DZ6_inWidth  = (DZ[5]-444.8-interStripC)/2; //for CR6C // (444.96 - 44.8 - 0.16)/2 = 0
- 
-    // mid angle of the sector
-    MidTile.push_back(0);     MidTile.push_back(0);
-    MidTile.push_back(0);     MidTile.push_back(0);
-    MidTile.push_back(0);     MidTile.push_back(0);
+    // Assume the edge boundaries are the same for all regions until get final geometry
+    for (int i = 0; i <3 ; ++i)	{
+    	for (int j = 0; j <3 ; ++j){
+			CRCEDGE1[i][j] = CEdge1[j];
+			CRCEDGE2[i][j] = CEdge2[j];
+		}
+    }
+
 }
 /**
  * Method to get digi hits based on x,y,z position and E
- * layers from 0 to 5
- *   layers 0,2,4 are Z detectors (inner)
- *   layers 1,3,5 are C detectors (outer)
  */
 vector<double>  bmt_strip::FindStrip(int layer, int sector, double x, double y, double z, double Edep)
 {
         // the return vector is always in pairs.
-        // The first number is the ID,
-        // the second number is the sharing percentage
-        vector<double> strip_id;
-        int NbStrips =0 ;
 
-        // dead zones
-        if(layer == 0 || layer == 1){
-                DZ_inLength = DZ4_inLength;
-                DZ_inWidth  = DZ4_inWidth;
-        }
-        else if(layer == 2 || layer == 3){
-                DZ_inLength = DZ5_inLength;
-                DZ_inWidth  = DZ5_inWidth;
-        }
-        else if(layer == 4 || layer == 5){
-                DZ_inLength = DZ6_inLength;
-                DZ_inWidth  = DZ6_inWidth;
-        }
-
-
-        // 1st define phi of the mean hit point
-        double phi;
-        if(x>0 && y>=0) phi = atan(y/x);
-        else if(x>0 && y<0) phi = 2.*Pi+atan(y/x);
-        else if(x<0) phi = Pi+atan(y/x);
-        else if(x==0 && y>0) phi = Pi/2.;
-        else if(x==0 && y<0) phi = 3.*Pi/2.;
-        else phi = 0; // x = y = 0, phi not defined
-
-        // now find the tile number (transverse diffusion will not change that)
-        int ti=0;
-        double theta_tmp=0;
-        for(int t=0; t<Nsector; t++)
-        {
-			theta_tmp = MidTile[layer]+2.*t*Pi/Nsector;
-			if(theta_tmp>2.*Pi) theta_tmp = theta_tmp - 2.*Pi;
-			if(theta_tmp<0) theta_tmp = theta_tmp + 2.*Pi;
-			if(fabs(phi-theta_tmp)<Pi/Nsector || fabs(2.*Pi-fabs(phi-theta_tmp))<Pi/Nsector) ti=t; // gives tile #
-        }
-
-        double phiij = MidTile[layer]+2.*ti*Pi/Nsector;
-        if(phi-phiij<=-Pi/Nsector) phi = phi+2.*Pi;
-        if(phi-phiij>Pi/Nsector) phi = phi-2.*Pi;
-        if(phi-phiij<=-Pi/Nsector || phi-phiij>Pi/Nsector) cout << "WARNING: incorrect phi value in BMT: " << phi*180./Pi << " vs " << phiij*180./Pi << endl;
-
-        // now compute the sigma of the (transverse) dispersion for this interaction
-        if(layer==1 || layer==3 || layer==5) sigma_td = sigma_td_max* sqrt((sqrt(x*x+y*y)-R[layer]+hStrip2Det)/hDrift); // "C" det, transverse diffusion grows with square root of distance
-        else sigma_td = sigma_td_max* sqrt((sqrt(x*x+y*y)-R[layer]+hStrip2Det)/(cos(theta_L)*hDrift)); // same, but "Z" detectors, so Lorentz angle makes drift distance longer by 1./cos(theta_L) . Means sigma_td can be larger than sigma_td_max
-        cout<<" x "<<x<<" y "<<y<<" z "<<z<<" phiij "<<phiij<<endl;
-        if(Edep>0)
-        {
-			NbStrips = Nstrips[layer];
-
-			if(layer%2==1)
-			{ //  for "C" layers, i.e. measuring z
-				vector<double> pitchC;
-				vector<double> widthC;
-				vector<int>nbunchC;
-
-				if(layer==1) {
-					pitchC = pitchC4;
-					widthC = widthC4;
-					nbunchC = nbunchC4;
-				}
-				if(layer==3) {
-					pitchC = pitchC5;
-					widthC = widthC5;
-					nbunchC = nbunchC5;
-				}
-				if(layer==5) {
-					pitchC = pitchC6;
-					widthC = widthC6;
-					nbunchC = nbunchC6;
-				}
-
-				double z_min = z-3*sigma_td; // minimum z in range
-				double z_max = z+3*sigma_td; // maximum z in range
-				cout<<" z_min "<<z_min <<" z_max "<<z_max<<endl;
-				double lowerBound = Z0[layer]+DZ_inWidth;
-				double upperBound = Z0[layer]+DZ[layer]-DZ_inWidth;
-
-				if(z>z_min && z_min<lowerBound)  	// the z_min falls outside of fiducial area
-					z_min=lowerBound;				// move z_min to the lower edge of the fiducial area
-				if(z<z_max && z_max>upperBound)  	// the z_max falls outside of fiducial area
-					z_max=upperBound;				// move z_max to the upper edge of the fiducial area
-
-				if(z_min>=lowerBound && z_max<=upperBound)
-				{
-					int min_strip = getNearestCstrip(z_min, layer, pitchC, nbunchC, NbStrips, DZ_inWidth);
-					int max_strip = getNearestCstrip(z_max, layer, pitchC, nbunchC, NbStrips, DZ_inWidth);
-					cout<<" min_strip "<<min_strip <<" max_strip "<<max_strip<<endl;
-					for(int s = min_strip; s<=max_strip; s++)
-					{
-						double Cz = getZasfcnCstrip(s, layer, pitchC, widthC, nbunchC);
-						double f =  getEnergyFraction(z, Cz, sigma_td);
-						cout<<" strip "<<s <<"  z "<< z<< " cz "<< Cz <<endl;
-						if(f>0.05)
-						{// 5% of total Edep cut off
-							strip_id.push_back(s);
-							strip_id.push_back(Edep*f);
-						}
-					}
-				} else {
-					strip_id.push_back(-1); // not in fiducial
-					strip_id.push_back(0);
-				}
-
-			}
-			if(layer%2==0)
-			{ //  for "Z" layers, i.e. measuring phi
-				double pitchZ =pitchZ6;
-
-				if(layer==0)
-					pitchZ = pitchZ4;
-				if(layer==2)
-					pitchZ = pitchZ5;
-				//if(layer==4)
-				//	pitchZ = pitchZ6;
-
-				double phi_min = phi + ((-3*sigma_td)/cos(theta_L)-(sqrt(x*x+y*y)-R[layer]+hStrip2Det)*tan(theta_L))/R[layer];
-				double phi_max = phi + ((3*sigma_td)/cos(theta_L)-(sqrt(x*x+y*y)-R[layer]+hStrip2Det)*tan(theta_L))/R[layer];
-				cout<<" phicorr min "<<(phi_min-phi)<<  " phicorr max "<<(phi_max-phi) <<endl;
-				double lowerBound = phiij-Pi/Nsector+DZ_inLength/R[layer] ;
-				double upperBound = phiij+Pi/Nsector-DZ_inLength/R[layer] ;
-				cout<<" layer "<< layer <<" pitchZ "<<pitchZ<<" pitchZ6 "<<pitchZ6<<endl;
-				if(phi>phi_min && phi_min<lowerBound)  	// the phi_min falls outside of fiducial area
-					phi_min=lowerBound;					// move phi_min to the lower edge of the fiducial area
-				if(phi<phi_max && phi_max>upperBound)  	// the phi_max falls outside of fiducial area
-					phi_max=upperBound;					// move phi_max to the upper edge of the fiducial area
-
-				if(phi_min>=lowerBound && phi_max<=upperBound)
-				{
-					int min_strip = getNearestZstrip(layer, phi_min, phiij, pitchZ, DZ_inLength);
-					int max_strip = getNearestZstrip(layer, phi_max, phiij, pitchZ, DZ_inLength);
-					cout<<" phi "<<phi <<" phi_min "<<phi_min<<" phi_max "<<phi_max<<endl;
-					cout<<" min_strip "<<min_strip<<" max_strip "<<max_strip<<endl;
-					for(int s = min_strip; s<=max_strip; s++)
-					{
-						double Cphi = getPhiasfcnCstrip(s, layer, phiij, pitchZ, DZ_inLength);
-						double f =  getEnergyFraction(0, Cphi, sigma_td);
-						if(f>0.05)
-						{// 5% of total Edep cut off
-							cout<<" strip "<<s <<"  Cphi "<< Cphi<<endl;
-							strip_id.push_back(s);
-							strip_id.push_back(Edep*f);
-						}
-					}
-				} else {
-					strip_id.push_back(-1); // not in fiducial
-					strip_id.push_back(0);
-				}
-			}
-        }
-		else
 		{ // Nel=0, consider the Edep is 0
 				strip_id.push_back(-1);
 				strip_id.push_back(0);
 		}
 
         return strip_id;
-}
-int bmt_strip::getNearestCstrip(double z, int layer, vector<double> pitchC, vector<int>nbunchC, int NbStrips, double DZ_inWidth){
-
-	int arraySize = nbunchC.size();
-	int ClosestStrip =-1;
-	double lowerBound = Z0[layer]+DZ_inWidth;
-	double upperBound = Z0[layer]+DZ[layer]-DZ_inWidth;
-	if(z==lowerBound)
-		ClosestStrip = 1;
-	if(z==upperBound)
-			ClosestStrip = NbStrips;
-	// if this z is within the first group of equal-pitch strips then get its closest strip
-	if(z-lowerBound>0 && z-lowerBound<nbunchC[0]*pitchC[0])
-			ClosestStrip = (int) (floor(((z-lowerBound)/pitchC[0]))+0.5);
-	// the closest strip is the effective z divided by the pitch +0.5
-	// if there is only one group -- stop here...
-	// otherwise loop over the number of groups of equal-pitch
-	// strips and find the corresponding nearest strip from z
-	int nstripsPrevGrp =0;
-	if(arraySize>1)
-		for(int i =1; i<arraySize; i++) {
-			nstripsPrevGrp+=i*nbunchC[i-1];
-			if(z-lowerBound>nbunchC[i-1]*pitchC[i-1] && z-lowerBound<nbunchC[i]*pitchC[i])
-					ClosestStrip = (int) (floor(((z-lowerBound)/pitchC[i]))+0.5+nstripsPrevGrp);
-		}
-	return ClosestStrip;
-}
-/**
- * A method to return the z position of a given C strip...
- */
-double bmt_strip::getZasfcnCstrip(int strip, int layer, vector<double> pitchC, vector<double> widthC, vector<int>nbunchC){
-
-	int num_strip = strip - 1;     			// index of the strip (starts at 0)
-
-	//For CRC, this function returns the Z position of the strip center
-	int group=0;
-	int limit=nbunchC[group];
-	double zc=Z0[layer]+widthC[group]/2.;
-
-	if (num_strip>0){
-	  for (int j=1;j<num_strip+1;j++){
-		zc+=widthC[group]/2.;
-		if (j>=limit) { //test if we change the width
-			group++;
-			limit+=widthC[group];
-		}
-		zc+=widthC[group]/2.+interStripC;
-	  }
-	}
-	return zc;
-
-
-}
-int bmt_strip::getNearestZstrip(int layer, double phi, double phiij, double pitchZ, double DZ_inLength) {
-	return (int) (floor(((R[layer]/pitchZ)*(phi-phiij+Pi/Nsector - (Inactivtheta[layer]/2.)*Pi/180. - DZ_inLength/R[layer]))+0.5));
-}
-
-double bmt_strip::getPhiasfcnCstrip(int s, int layer, double phiij, double pitchZ, double DZ_inLength) {
-	return (s - 0.5)*pitchZ/R[layer]+phiij-Pi/Nsector + (Inactivtheta[layer]/2.)*Pi/180. + DZ_inLength/R[layer];
-}
-double bmt_strip::getEnergyFraction(double z0, double z, double sigma){
-	double pdf_gaussian = (1./(sigma*sqrt(2*Pi)))* exp( -0.5*((z-z0)/sigma)*((z-z0)/sigma) );
-	return pdf_gaussian;
 }
--- hitprocess/clas12/micromegas/bmt_strip.h
+++ hitprocess/clas12/micromegas/bmt_strip.h
@@ -8,66 +8,49 @@ using namespace std;
 /// \class bmt_strip
 /// <b> bmt_strip </b>\n\n
 /// Micromegas strip finding routine\n
-/// \author \n S. Procureu, G. Charles, M. Ungaro
-/// \author mail: sebastien.procureur@cea.fr, gabriel.charles@cea.fr, ungaro@jlab.org\n\n\n
 
 
 class bmt_strip
 {
 	public:
-		int Nsector;
+
 		double Pi;
-
-		double interlayer;       // distance between 2 layers of a superlayer
-		vector<double> Z0;       // z of the upstream part of the layer
-		vector<double> DZ;       // total z length of the layer
-		vector<double> R;        // radii of layers
-		vector<double> MidTile;  // mid angle of the sector
-		vector<int> Nstrips;     // number of strips in each detector
 		
-		double interStripZ; // interstrip for CRnZ
-		double interStripC; // interstrip for CRnC
-		double pitchZ4;  // pitch for Z4
-		double pitchZ5;  // pitch for Z5
-		double pitchZ6;  // pitch for Z6
-		vector<double> pitchC4;  // pitch for C4
-		vector<double> pitchC5;  // pitch for C5
-		vector<double> pitchC6;  // pitch for C6
-		vector<double> widthC4;  // strip with for C4
-		vector<double> widthC5;  // strip with for C5
-		vector<double> widthC6;  // strip with for C6
-		vector<int> nbunchC4; // number of strip bunches for C4
-		vector<int> nbunchC5; // number of strip bunches for C5
-		vector<int> nbunchC6; // number of strip bunches for C6
-		vector<double> Inactivtheta; // dead angle because of mecanics
-		double DZ_inLength; // size of the band of dead zones all around in the length of the card
-		double DZ_inWidth; // size of the band of dead zones all around in the width of the card
-		double DZ4_inLength; // size of the band of dead zones all around in the length of the card (for CR4Z)
-		double DZ4_inWidth; // size of the band of dead zones all around in the width of the card (for CR4C)
-		double DZ5_inLength; // size of the band of dead zones all around in the length of the card (for CR5Z)
-		double DZ5_inWidth; // size of the band of dead zones all around in the width of the card (for CR5C)
-		double DZ6_inLength; // size of the band of dead zones all around in the length of the card (for CR6Z)
-		double DZ6_inWidth; // size of the band of dead zones all around in the width of the card (for CR6C)
+		// THE GEOMETRY CONSTANTS
+		int NREGIONS = 3  ;					// 3 regions of MM
 		
+		//Z detector characteristics
+		double CRZRADIUS[NREGIONS] ; 		// the radius of the Z detector in mm
+		int CRZNSTRIPS[NREGIONS] ; 			// the number of strips
+		double CRZSPACING[NREGIONS] ; 		// the strip spacing in mm
+		double CRZWIDTH[NREGIONS] ; 		// the strip width in mm
+		double CRZLENGTH[NREGIONS] ; 		// the strip length in mm
+		double CRZZMIN[NREGIONS] ; 			// PCB upstream extremity mm
+		double CRZZMAX[NREGIONS] ; 			// PCB downstream extremity mm
+		double CRZOFFSET[NREGIONS] ; 		// Beginning of strips in mm
+		vector<vector<double> >  CRZEDGE1; 	// the angle of the first edge of each PCB detector A, B, C
+		vector<vector<double> >  CRZEDGE2; 	// the angle of the second edge of each PCB detector A, B, C
+		double CRZXPOS[NREGIONS]; 			// Distance on the PCB between the PCB first edge and the edge of the first strip in mm
 
-		double hDrift;           // Size of the drift gap
-		double hStrip2Det;       // distance between strips and the middle of the conversion gap (~half the drift gap)
-		double sigma_td_max;     // maximum value of the transverse diffusion
-		double sigma_td;         // current value of the transverse diffusion
-		double theta_L;          // Lorentz angle (for Z detectors)
-		double w_i;              // mean ionization potential
-		int Nel;                 // number of electrons (Nt) for a given hit
-		double z_real, phi_real; // z and phi of the hit after transverse diffusion
+		//C detector characteristics
+		double CRCRADIUS[NREGIONS]; 		// the radius of the Z detector in mm
+		int CRCNSTRIPS[NREGIONS]; 			// the number of strips
+		double CRCSPACING[NREGIONS]; 		// the strip spacing in mm
+		double CRCLENGTH[NREGIONS]; 		// the strip length in mm
+		double CRCZMIN[NREGIONS]; 			// PCB upstream extremity mm
+		double CRCZMAX[NREGIONS]; 			// PCB downstream extremity mm
+		double CRCOFFSET[NREGIONS]; 		// Beginning of strips in mm
+		vector<vector<int> >  CRCGROUP; 	// Number of strips with same width
+		vector<vector<double> >  CRCWIDTH;	// the width of the corresponding group of strips
+		vector<vector<double> >  CRCEDGE1; 	// the angle of the first edge of each PCB detector A, B, C
+		vector<vector<double> >  CRCEDGE2; 	// the angle of the second edge of each PCB detector A, B, C
+		double CRCXPOS[NREGIONS]; 			// Distance on the PCB between the PCB first edge and the edge of the first strip in mm
 
+		// THE SIMULATION CONSTANTS
 
-		double x,y,z;            // z of the track is redefined in FindCard. Units are microns - input are millimiters 
 		void fill_infos(); 
 		vector<double> FindStrip( int layer, int sector, double x, double y, double z, double Edep);   // Strip Finding Routine
-		int getNearestCstrip(double z, int layer, vector<double> pitchC, vector<int>nbunchC, int NbStrips, double DZ_inWidth);
-		double getZasfcnCstrip(int strip, int layer, vector<double> pitchC, vector<double> widthC, vector<int>nbunchC);
-		int getNearestZstrip(int layer, double phi, double phiij, double pitchZ, double DZ_inLength) ;
-		double getPhiasfcnCstrip(int s, int layer, double phiij, double pitchZ, double DZ_inLength) ;
-		double getEnergyFraction(double z0, double z, double sigma);
+
 };
 
 #endif
