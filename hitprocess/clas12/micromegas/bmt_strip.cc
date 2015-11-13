3fdab7a1c9450509384bc68885e389853f830703
Veronique Ziegler <ziegler@jlab.org>
2015-11-13 10:56:05 -0500

    ...

--- hitprocess/clas12/micromegas/bmt_strip.cc
+++ hitprocess/clas12/micromegas/bmt_strip.cc
@@ -183,8 +183,8 @@ vector<double> bmt_strip::FindStrip(int layer, int sector, double x, double y, d
 				for(int s = min_strip; s < max_strip+1; s++)
 				{
 					//corresponding phi value between +/-3sigmas
-					double phi_0 =  CRZStrip_GetPhi( sector, layer, getZStrip(layer, phi))*CRZRADIUS[num_region]+Delta_rad*tan(ThetaL))*cos(ThetaL);
-					double phi_s =  CRZStrip_GetPhi( sector, layer, s)*CRZRADIUS[num_region]+Delta_rad*tan(ThetaL))*cos(ThetaL);
+					double phi_0 =  ( CRZStrip_GetPhi( sector, layer, getZStrip(layer, phi))*CRZRADIUS[num_region]+Delta_rad*tan(ThetaL) )*cos(ThetaL);
+					double phi_s =  ( CRZStrip_GetPhi( sector, layer, s)*CRZRADIUS[num_region]+Delta_rad*tan(ThetaL) )*cos(ThetaL);
 					double f = getEnergyFraction(0, phi_s-phi0, sigma);
 					strip_id.push_back(s);
 					strip_id.push_back(f); // no gain fluctuation yet
