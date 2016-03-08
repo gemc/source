950187c4df9db8edf1f79fd580ef67d722e1bc2c
Cole Smith <lcs1956@me.com>
2016-03-07 21:40:14 -0500

    Added CCDB constants processing to CTOF, made CTOF,FTOF CCDB implementation consistent with documentation, fixed ADC smearing bug in ECAL,PCAL

--- hitprocess/clas12/pcal_hitprocess.cc
+++ hitprocess/clas12/pcal_hitprocess.cc
@@ -18,8 +18,7 @@ static pcConstants initializePCConstants(int runno)
 	// do not initialize at the beginning, only after the end of the first event,
 	// with the proper run number coming from options or run table
 	if(runno == -1) return pcc;
-	
-	
+		
 	int isec,ilay,istr;
 	
 	// database
@@ -28,14 +27,22 @@ static pcConstants initializePCConstants(int runno)
 	pcc.connection = "mysql://clas12reader@clasdb.jlab.org/clas12";
 	pcc.variation  = "default";
 	
-	pcc.attl                = 3760.;  // Attenuation Length (mm)
 	pcc.TDC_time_to_evio    = 1000.;  // Currently EVIO banks receive time from rol2.c in ps (raw counts x 24 ps/chan. for both V1190/1290), so convert ns to ps.
 	pcc.ADC_MeV_to_evio     = 10.  ;  // MIP based calibration is nominally 10 channels/MeV
-	pcc.PE_yld              = 11.5 ;  // Number of p.e. divided by the energy deposited in MeV. See EC NIM paper table 1.
 	pcc.veff                = 160. ;  // Effective velocity of scintillator light (mm/ns)
+	pcc.pmtPEYld            = 11.5 ; // Number of p.e. divided by the energy deposited in MeV. See EC NIM paper table 1.
+	pcc.pmtQE               = 0.27 ; 
+	pcc.pmtDynodeGain       = 4.0  ; 
+        pcc.pmtDynodeK          = 0.5  ; // K=0 (Poisson) K=1(exponential)	vector<vector<double> > data;
+	//  Fluctuations in PMT gain distributed using Gaussian with
+	//  sigma=1/SNR where SNR = sqrt[(1-QE+(k*del+1)/(del-1))/npe] del = dynode gain k=0-1
+	//  Adapted from G-75 (pg. 169) and and G-111 (pg. 174) from RCA PMT Handbook.
+	//  Factor k for dynode statistics can range from k=0 (Poisson) to k=1 (exponential).
+	//  Note: GSIM sigma was incorrect (used 1/sigma for sigma).
+	pcc.pmtFactor           = sqrt(1-pcc.pmtQE+(pcc.pmtDynodeK*pcc.pmtDynodeGain+1)/(pcc.pmtDynodeGain-1));
 
 	vector<vector<double> > data;
-	auto_ptr<Calibration> calib(CalibrationGenerator::CreateCalibration(pcc.connection));
+        auto_ptr<Calibration> calib(CalibrationGenerator::CreateCalibration(pcc.connection));
 	
 	sprintf(pcc.database,"/calibration/ec/attenuation:%d",pcc.runNo);
 	data.clear(); calib->GetCalib(data,pcc.database);
@@ -120,20 +127,20 @@ map<string, double> pcal_HitProcess :: integrateDgt(MHit* aHit, int hitn)
 	int TDC = 0;
 	
 	// simulate the adc value.
-	if (tInfos.eTot > 0)
-	{
-		double PC_npe = G4Poisson(Etota*pcc.PE_yld); //number of photoelectrons
-																	//  Fluctuations in PMT gain distributed using Gaussian with
-																	//  sigma SNR = sqrt(ngamma)/sqrt(del/del-1) del = dynode gain = 3 (From RCA PMT Handbook) p. 169)
-																	//  algorithm, values, and comment above taken from gsim.
-		double sigma = sqrt(PC_npe)/1.22;
-		double PC_MeV = G4RandGauss::shoot(PC_npe,sigma)*pcc.ADC_MeV_to_evio/pcc.PE_yld;
-		if (PC_MeV <= 0) PC_MeV=0.0; // guard against weird, rare events.
-		ADC = (int) PC_MeV;
+	if (Etota > 0) {
+	  double PC_npe = G4Poisson(Etota*pcc.pmtPEYld); //number of photoelectrons
+	  if (PC_npe>0) {
+	    double sigma = pcc.pmtFactor/sqrt(PC_npe);
+	    double PC_MeV = G4RandGauss::shoot(PC_npe,sigma)*pcc.ADC_MeV_to_evio/pcc.pmtPEYld;
+	    if (PC_MeV>0) {
+	      ADC = (int) PC_MeV;
+	      TDC = (int) ((tInfos.time+Ttota/tInfos.nsteps)*pcc.TDC_time_to_evio);
+	    }
+	  }
 	}
 	
-	// simulate the tdc.
-	TDC = (int) ((tInfos.time+Ttota/tInfos.nsteps)*pcc.TDC_time_to_evio);
+	// EVIO banks record time with offset determined by position of data in capture window.  On forward carriage this is currently
+	// around 1.4 us.  This offset is omitted in the simulation.
 	
 	dgtz["hitn"]   = hitn;
 	dgtz["sector"] = sector;
