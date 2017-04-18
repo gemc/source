09e9b20d9046695ca3493c01d9472d30c1750853
raffaelladevita <devita@ge.infn.it>
2017-04-18 19:23:34 +0200

    new digitization routines for ftm with correct strip ids

--- hitprocess/clas12/micromegas/ftm_strip.cc
+++ hitprocess/clas12/micromegas/ftm_strip.cc
@@ -4,39 +4,8 @@
 #include <iostream>
 #include <cmath>
 
-/*
-void ftm_strip::fill_infos(detector Detector)
-{
-  // all dimensions are in mm
- 
-  Pi  = 3.14159265358;
-  interlayer      = (0.3/2.+0.1+0.015)*2.; // distance between 2 layers of a superlayer
-  intersuperlayer = 20.0;      // distance between 2 superlayers
-  
-  pitch           = 0.500;                         // pitch of the strips
-  hDrift          = 5.35;
-  hStrip2Det      = hDrift/2.;
-  sigma_td_max    = 0.0; // very small transverse diffusion (temporary)
-  w_i             = 25.0;
-  
-  Rmin = 65.0;                 // inner radius of disks
-  Rmax = 142.0;  // outer radius of disks
-  Z_1stlayer = 1773.-0.3/2.-0.1-0.015; // z position of the 1st layer : epoxy center - half epoxy Dz - PCB Dz - strips Dz
-  
-    cout << Detector.name << " " << Detector.dimensions.size();
-    for(int i=0; i<Detector.dimensions.size(); i++) cout << " " << Detector.dimensions[i];
-    cout << endl;
-  // z of the upstream part of the layer
-  Z0.push_back(Z_1stlayer);
-  Z0.push_back(Z0[0]+interlayer);
-  Z0.push_back(Z_1stlayer+intersuperlayer);
-  Z0.push_back(Z0[2]+interlayer);
-  
-  // Number of strips
-  Nstrips = (int) floor(2.*(Rmax+Rmin)/pitch);
-}
-*/
-    
+
+
 vector<double> ftm_strip::FindStrip(int layer, double x, double y, double z, double Edep, detector Detector,ftmConstants ftmcc)
 {
     // the return vector is always in pairs.
@@ -46,69 +15,139 @@ vector<double> ftm_strip::FindStrip(int layer, double x, double y, double z, dou
     // number of electrons (Nt)
     int Nel = (int) (1e6*Edep/ftmcc.w_i);
     
+    double r = sqrt(x*x+y*y);
+
     // get layer position and drift distance from the detector dimensions
-    double z0 = -Detector.dimensions[2];
-    double hdrift = Detector.dimensions[2]*2;
+    double z0           = -Detector.dimensions[2];
+    double hdrift       =  Detector.dimensions[2]*2;
 //    cout << Detector.name << " " << Detector.dimensions.size();
 //    for(int i=0; i<Detector.dimensions.size(); i++) cout << " " << Detector.dimensions[i];
 //    cout << endl;
 //    cout << " z0 " <<z0 << " drift " << hdrift << " z " << z << endl;
-
+//    cout << " layer " << layer << " x " << x << " y " << y << " z " << z << " nel " << Nel << endl;
     // old if(fabs(z-Z0[layer])>(hDrift+0.2)) cout << "Warning! z position of the FTM hit is not in the sensitive volume: " << z-Z0[layer]<< endl;
-    if(fabs(z-z0)>(hdrift+0.2)) cout << "Warning! z position of the FTM hit is not in the sensitive volume: " << z << " " << z0<< endl;
+    if(fabs(z-z0)>(hdrift+0.2)) cout << "Warning! z position of the FTM hit is not in the sensitive volume: " << z-z0<< endl;
+    if(r<Detector.dimensions[0] || r>Detector.dimensions[1]) cout << "Warning! r position of the FTM hit is not in the sensitive volume: " << r << endl;
+  
+    double sigma_td_max = ftmcc.sigma_0*sqrt(hdrift/cm);
+    double sigma_td     = sigma_td_max* sqrt(fabs(z-z0)/hdrift); // expression without Lorentz angle
+  
+    // check if hit is within active area
     
-    double sigma_td = ftmcc.sigma_td_max* sqrt(fabs(z-z0)/hdrift); // expression without Lorentz angle
-//    cout << Nel << endl;
-    int ClosestStrip=0;
-    if(Nel>0)
-    {
-      for(int iel=0;iel<Nel;iel++)
-	{ // loop over (total) electrons
+    if(r<ftmcc.rmax && r>ftmcc.rmin) {
         double x_real=x;
         double y_real=y;
-	  if(layer%2==0)
-	    {
-          x_real = (double) (G4RandGauss::shoot(x,sigma_td));
-          y_real = y;
-	      if(-ftmcc.rmin<x_real && x_real<ftmcc.rmin && y_real<0) ClosestStrip = (int) (floor(2.0*ftmcc.rmax/ftmcc.pitch+(x_real+ftmcc.rmin)/ftmcc.pitch+0.5));
-	      else ClosestStrip = (int) (floor((x_real+ftmcc.rmax)/ftmcc.pitch+0.5));
-	    }
-	  if(layer%2==1)
-	    {
-	      x_real = x;
-	      y_real = (double) (G4RandGauss::shoot(y,sigma_td));
-	      if(-ftmcc.rmin<y_real && y_real<ftmcc.rmin && x_real<0) ClosestStrip = (int) (floor(2.0*ftmcc.rmax/ftmcc.pitch+(y_real+ftmcc.rmin)/ftmcc.pitch+0.5));
-	      else ClosestStrip = (int) (floor((y_real+ftmcc.rmax)/ftmcc.pitch+0.5));
-	    }
-	  
-	  if(sqrt(x_real*x_real+y_real*y_real)<ftmcc.rmax && sqrt(x_real*x_real+y_real*y_real)>ftmcc.rmin && ClosestStrip>=0 && ClosestStrip<=ftmcc.nstrips)
-	    { // strip is in the acceptance
-	      for(int istrip=0;istrip< (int) (strip_id.size()/2);istrip++)
-		{
-		  if(strip_id[2*istrip]==ClosestStrip)
-		    {// already hit strip - add Edep
-		      strip_id[2*istrip+1]=strip_id[2*istrip+1]+1./((double) Nel); // no gain fluctuation yet
-		      ClosestStrip=-1; // not to use it anymore
-		    }
-		}
-	      if(ClosestStrip>-1)
-		{ // this is a new strip
-		  strip_id.push_back(ClosestStrip);
-		  strip_id.push_back(1./((double) Nel)); // no gain fluctuation yet
-		}
-	    }
-	  else
-	    {// not in the acceptance
-	      strip_id.push_back(-1);
-	      strip_id.push_back(1);
-	    }
-	}
+        if (layer%2==0){ // y_real is the coordinate given by the detector; strips are along x_real
+            x_real = y;
+            y_real = x;
+        }
+        if (layer%2==1){  //y_real is the coordinate given by the detector; strips are along x_real
+            x_real = x;
+            y_real = y;
+        }
+//        cout << " nel " << Nel << " x_real " << x_real << " y_real " << y_real << " z " << z << endl;
+        
+        
+        // calculate the y coordinate of closest strip (N.B. y_strip may exceed the actual strip range
+        // because the active area is a bit larger than the one instrumented with strips)
+        double y_strip = ftmcc.pitch*(floor(y_real/ftmcc.pitch))+ftmcc.pitch/2;
+//        cout << " nel " << Nel << " x_real " << x_real << " y_strip " << y_strip << " z " << z << endl;
+        
+        // calculates y coordinates of strips that may be involved in the cluster
+        double weight_tot=0;
+        for(int i=-ftmcc.nb_sigma; i<=ftmcc.nb_sigma; i++) {
+            
+            // calculate strip coordinate
+            double iy = y_strip+i*ftmcc.pitch;
+            
+            // determine the strip number
+            int istrip = get_strip_ID(x_real,iy,ftmcc);
+            // check if strip number is well defined (i.e. now really inside the active volume)
+            if(istrip>0) {
+                // calculate x as mid point of the strip and the strip length
+                double ix = get_strip_X(x_real,iy,ftmcc);
+                double strip_L = get_strip_L(ix,iy,ftmcc);
+                // calculate weight
+                double weight= exp(-(iy-y_real)*(iy-y_real)/2/sigma_td/sigma_td);
+                int weight_n = round(weight*Nel*10);   // round weight to closer integer to get rid of very low energy hits
+                if(weight_n>0 || i==0) {               // save strips with non zero weight or at least the central one
+                    strip_id.push_back(istrip);
+                    strip_id.push_back(weight);
+                    weight_tot += weight;
+                }
+            }
+        }
+//        cout << strip_id.size() << endl;
+        if(strip_id.size()<2) {    // if not strips have been identified, set at least 1
+            strip_id.push_back(-1);
+            strip_id.push_back(1);
+        }
+        // normalize weight to have integral equal to 1
+        for (int i=0; i<strip_id.size()/2; i++) {
+            strip_id[i*2+1]=strip_id[i*2+1]/weight_tot;
+//            cout << strip_id[i*2] << " " << strip_id[i*2+1] << endl;
+        }
     }
-  else
-    { // Nel=0, consider the Edep is 0
-      strip_id.push_back(-1);
-      strip_id.push_back(1);
+    else {  //  return strip id -1 is hit is outside active area
+        strip_id.push_back(-1);
+        strip_id.push_back(1);
     }
+ 
   return strip_id;
 }
 
+
+int ftm_strip::get_strip_ID(double x, double y,ftmConstants ftmcc) {
+    double r=sqrt(x*x+y*y);
+    if(r<ftmcc.rmax && r>ftmcc.rmin && fabs(y)<ftmcc.pitch*ftmcc.nstrips*2/6) {
+        int strip = (int) floor(y/ftmcc.pitch) + 1 + ftmcc.nstrips*2/6;
+        if(strip>ftmcc.nstrips*3/6) { // strip in the top sector
+            strip += ftmcc.nstrips*2/6;
+        }
+        else if(strip>ftmcc.nstrips/6 && x>0){
+            strip += ftmcc.nstrips*2/6;
+        }
+        return strip;
+    }
+    else {
+        return -1;
+    }
+}
+
+double ftm_strip::get_strip_X(double x, double y,ftmConstants ftmcc) {
+    int istrip = get_strip_ID(x,y,ftmcc);
+    double strip_X = 0;
+    if(istrip<ftmcc.nstrips/6 || istrip>ftmcc.nstrips*5/6) {
+        strip_X = 0;
+    }
+    else {
+        strip_X=(ftmcc.rmax*sin(acos(fabs(y)/ftmcc.rmax))+ftmcc.rmin*sin(acos(fabs(y)/ftmcc.rmin)))/2;
+        if(x<0) strip_X = -strip_X;
+    }
+    return strip_X;
+}
+
+double ftm_strip::get_strip_L(double x, double y,ftmConstants ftmcc) {
+    int istrip = get_strip_ID(x,y,ftmcc);
+    double strip_L = 0;
+    if(istrip<ftmcc.nstrips/6 || istrip>ftmcc.nstrips*5/6) {
+        strip_L = ftmcc.rmax*sin(acos(fabs(y)/ftmcc.rmax));
+    }
+    else {
+        strip_L = ftmcc.rmax*sin(acos(fabs(y)/ftmcc.rmax))-ftmcc.rmin*sin(acos(fabs(y)/ftmcc.rmin));
+    }
+    return strip_L;
+}
+
+
+
+/*
+ double ftm_strip::Weight_td(double strip_x, double strip_y, double x, double y, double z,sigma_td,ftmConstants ftmcc){
+//  Carac_strip(strip);
+    double strip_L = get_strip_L(strip_x,strip_y,ftmcc);
+    double wght=(erf((strip_y+ftmcc.pitch/2.-y)/sigma_td/sqrt(2))-erf((strip_y-ftmcc.pitch/2.-y)/sigma_td/sqrt(2)))
+               *(erf((strip_x+strip_L/2.-x)/sigma_td/sqrt(2))-erf((strip_x-strip_L/2.-x)/sigma_td/sqrt(2)))/2./2.;
+    if (wght<0) wght=-wght; //Did not test if it happens... should not but we never know. If not tested, better be sure.
+    return wght;
+}
+ */
--- hitprocess/clas12/micromegas/ftm_strip.h
+++ hitprocess/clas12/micromegas/ftm_strip.h
@@ -25,14 +25,14 @@ public:
     string connection;
     char   database[80];
     
-    double sigma_td_max ;  // // very small transverse diffusion (temporary)
+    double sigma_0      ;  // transverse diffusion
     double w_i          ;  // ionization energy
     
     double rmin ;          // inner radius of disks
     double rmax ;          // outer radius of disks
     double pitch;          // strip pitch
     int    nstrips;        // number of strips per layer
-    
+	int    nb_sigma;       // To define the number of strips to look at around the closest one
     //	voltage signal parameters, using double gaussian + delay (function DGauss, need documentation for it)
     double vpar[4];
 };
@@ -41,33 +41,45 @@ public:
 class ftm_strip
 {
 public:
-/*
+    /*
 	double pitch;
 	double Pi;
 
 	double interlayer;       // distance between 2 layers of a superlayer
 	double intersuperlayer;  // distance between 2 superlayers
-	double Rmin;             // inner radius of disks
-	double Rmax;             // outer radius of disks
-//	double Z_1stlayer;       // z position of the 1st layer
+	double R_min;             // inner radius of disks
+	double R_max;             // outer radius of disks
+	double Z_1stlayer;       // z position of the 1st layer
 
 	vector<double> Z0;       // z of the upstream part of the layer
 	vector<double> R;        // radii of layers
 	vector<double> MidTile;  // mid angle of the sector
-	int Nstrips;             // Number of strips for 1 card
-//	double hDrift;           // Size of the drift gap
-//	double hStrip2Det;       // distance between strips and the middle of the conversion gap (~half the drift gap)
+	int N_str;             // Number of strips for 1 card
+	int N_halfstr;             // Number of strips cut in the middle
+	int N_sidestr;             // Number of strips on one side, running from negative to positive x (unlike the half strips)
+	double y_central;        // Limit the central part for strip finding
+	double xc,yc;            // Center of the Closest strip
+	double hDrift;           // Size of the drift gap
+	double hStrip2Det;       // distance between strips and the middle of the conversion gap (~half the drift gap)
 	double sigma_td_max;     // maximum value of the transverse diffusion
 	double sigma_td;         // current value of the transverse diffusion
 	double w_i;              // mean ionization potential
 	int Nel;                 // number of electrons (Nt) for a given hit
-	double x_real, y_real; // x,y of the hit after transverse diffusion
- */
-//	void fill_infos(detector);
+	double strip_x;          // strip_x is the middle of the strips X-coord
+	double strip_y;          // strip_y is the position of the strips
+	double strip_length;     // length of the strip
+	double y_real; 		 // y position in the detector coordinate system
+	double x_real;		 // x position in the detector coordinate system
+	int nb_sigma;            // To define the number of strips to look at around the closest one
+     */
+//	void fill_infos();
 
 	vector<double> FindStrip( int layer, double x, double y, double z, double Edep, detector Detector,ftmConstants ftmcc);   // Strip Finding Routine
-
+    int    get_strip_ID(double x, double y,ftmConstants ftmcc);   // return strip number based on coordinate
+    double get_strip_X(double x, double y,ftmConstants ftmcc);    // return strip x coordinate
+    double get_strip_L(double x, double y,ftmConstants ftmcc);    // return strip length
+//	void Carac_strip(int strip); //length and position of a strip
+//	double Weight_td(double xs, double ys, double x, double y, double z,ftmConstants ftmcc); //Compute the fraction of Nel falling onto the strip, depending on x,y in the FMT coordinate system
 };
 
 #endif
-
