a5cc5b2ba417ef5a63d8e4387d4100c3630a945c
Maurik Holtrop <maurik@physics.unh.edu>
2021-04-07 17:38:20 -0400

    Change how GEANT4 is build. Needs to be two step.

--- cmake/Geant4.cmake
+++ cmake/Geant4.cmake
@@ -21,19 +21,18 @@ if(NOT Geant4_FOUND)
         GIT_TAG          v${Geant4_VERSION}
         GIT_SHALLOW      ON
         SOURCE_DIR       ${CMAKE_BINARY_DIR}/Geant4
-        CMAKE_ARGS       -DCMAKE_INSTALL_PREFIX=${Geant4_INSTALL_DIR} -DGEANT4_INSTALL_DATA=ON -DGEANT4_USE_SYSTEM_EXPAT=OFF -DGEANT4_INSTALL_EXAMPLES=OFF -DGEANT4_USE_QT=ON
+        CMAKE_ARGS       -DCMAKE_INSTALL_PREFIX=${Geant4_INSTALL_DIR}
+                         -DGEANT4_USE_GDML=ON
+                         -DGEANT4_USE_QT=ON
+                         -DGEANT4_INSTALL_DATA=ON
+                         -DGEANT4_USE_SYSTEM_EXPAT=OFF
+                         -DGEANT4_INSTALL_EXAMPLES=OFF
+                         -DGEANT4_USE_QT=ON
         BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM} -j4
         UPDATE_COMMAND   ""
     )
-    # vis options left out for now
-    # -DGEANT4_USE_OPENGL_X11=${GEANT4_USE_OPENGL_X11} -DGEANT4_USE_QT=${GEANT4_USE_QT}
-    set(Geant4_DIR ${Geant4_INSTALL_DIR}/lib/Geant4-${Geant4_VERSION} CACHE PATH "Geant4 install dir" FORCE)
-    set(Geant4_INCLUDE_DIRS ${Geant4_INSTALL_DIR}/include/Geant4 CACHE PATH "Geant4 include dirs" FORCE)
-    set(Geant4_LIBRARIES "Geant4::G4Tree;Geant4::G4FR;Geant4::G4GMocren;Geant4::G4visHepRep;Geant4::G4RayTracer;Geant4::G4VRML;Geant4::G4OpenGL;Geant4::G4gl2ps;Geant4::G4vis_management;Geant4::G4modeling;Geant4::G4interfaces;Geant4::G4persistency;Geant4::G4analysis;Geant4::G4error_propagation;Geant4::G4readout;Geant4::G4physicslists;Geant4::G4run;Geant4::G4event;Geant4::G4tracking;Geant4::G4parmodels;Geant4::G4processes;Geant4::G4digits_hits;Geant4::G4track;Geant4::G4particles;Geant4::G4geometry;Geant4::G4materials;Geant4::G4graphics_reps;Geant4::G4intercoms;Geant4::G4global;Geant4::G4clhep;Geant4::G4expat;Geant4::G4zlib;Geant4::G4UIVisDefinitions")
+    SET( DEPENDENCIES_RERUN_CMAKE "YES" FORCE )
 else()
     # add_custom_target(Geant4) # dummy target
     message(STATUS "Geant4 was found at: ${Geant4_DIR}")
-    foreach(_print_item Geant4_INCLUDE_DIRS Geant4_LIBRARIES XercesC_FOUND XercesC_INCLUDE_DIR XercesC_LIBRARY_DEBUG XercesC_LIBRARY_RELEASE)
-        message(STATUS "${_print_item}  = ${${_print_item}}")
-    endforeach()
 endif()
--- cmake/mlibrary.cmake
+++ cmake/mlibrary.cmake
@@ -1,54 +1,61 @@
 #
 message(STATUS "Checking for mlibrary")
-find_package(mlibrary QUIET)
-if(NOT mlibrary_FOUND)
-    message(STATUS "**********************************************************************")
-    message(STATUS "** mlibrary was not found, so it will be installed.                 **")
-    message(STATUS "** OR set CMAKE_PREFIX_PATH to the location of mlibraryConfig.cmake **")
-    message(STATUS "** and rerun cmake.                                                 **")
-    message(STATUS "**********************************************************************")
-    set(MLIBRARY_VERSION iss197 CACHE STRING "mlibrary version" FORCE)
-    externalproject_add(
-            mlibrary_external
-            GIT_REPOSITORY   "https://github.com/mholtrop/mlibrary.git"
-            GIT_TAG          ${MLIBRARY_VERSION}
-            SOURCE_DIR       ${CMAKE_BINARY_DIR}/mlibrary
-            INSTALL_DIR      ${CMAKE_INSTALL_PREFIX}
-            CMAKE_ARGS       -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-            BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM} install
-            UPDATE_COMMAND   ""
-    )
-    add_library(mlibrary SHARED IMPORTED)
-    add_dependencies(dependencies mlibrary)
-    add_dependencies(mlibrary mlibrary_external)
-    file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/include/mlibrary)
-    file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/include/assimp)
-    set_target_properties(mlibrary PROPERTIES
-                          INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include/mlibrary;${CMAKE_INSTALL_PREFIX}/include"
-                          INTERFACE_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib"
-                          INTERFACE_LINK_LIBRARIES "${Geant4_LIBRARIES};Qt5::Xml;Qt5::Widgets;Qt5::Sql;cadmesh"
-                          )
-    set_property(TARGET mlibrary APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-    set_target_properties(mlibrary PROPERTIES
-                          IMPORTED_LOCATION_RELEASE "${CMAKE_INSTALL_PREFIX}/lib/libmlibrary.dylib"
-                          IMPORTED_SONAME_RELEASE "@rpath/libmlibrary.dylib"
-                          )
+if(NOT Geant4_FOUND)
+    message(STATUS "*********************************************************")
+    message(STATUS "* mlibrary will not be build or configured until GEANT4 *")
+    message(STATUS "* is found. Make sure GEANT4 builds, then rerun cmake   *")
+    message(STATUS "*********************************************************")
+else()
+    find_package(mlibrary QUIET)
+    if(NOT mlibrary_FOUND)
+        message(STATUS "**********************************************************************")
+        message(STATUS "** mlibrary was not found, so it will be installed.                 **")
+        message(STATUS "** OR set CMAKE_PREFIX_PATH to the location of mlibraryConfig.cmake **")
+        message(STATUS "** and rerun cmake.                                                 **")
+        message(STATUS "**********************************************************************")
+        set(MLIBRARY_VERSION iss197 CACHE STRING "mlibrary version" FORCE)
+        externalproject_add(
+                mlibrary_external
+                GIT_REPOSITORY   "https://github.com/mholtrop/mlibrary.git"
+                GIT_TAG          ${MLIBRARY_VERSION}
+                SOURCE_DIR       ${CMAKE_BINARY_DIR}/mlibrary
+                INSTALL_DIR      ${CMAKE_INSTALL_PREFIX}
+                CMAKE_ARGS       -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+                BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM} install
+                UPDATE_COMMAND   ""
+        )
+        add_library(mlibrary SHARED IMPORTED)
+        add_dependencies(dependencies mlibrary)
+        add_dependencies(mlibrary mlibrary_external)
+        file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/include/mlibrary)
+        file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/include/assimp)
+        set_target_properties(mlibrary PROPERTIES
+                              INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include/mlibrary;${CMAKE_INSTALL_PREFIX}/include"
+                              INTERFACE_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib"
+                              INTERFACE_LINK_LIBRARIES "${Geant4_LIBRARIES};Qt5::Xml;Qt5::Widgets;Qt5::Sql;cadmesh"
+                              )
+        set_property(TARGET mlibrary APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+        set_target_properties(mlibrary PROPERTIES
+                              IMPORTED_LOCATION_RELEASE "${CMAKE_INSTALL_PREFIX}/lib/libmlibrary.dylib"
+                              IMPORTED_SONAME_RELEASE "@rpath/libmlibrary.dylib"
+                              )
 
-    add_library(mlibrary_static STATIC IMPORTED)
-    add_dependencies(dependencies mlibrary_static)
-    add_dependencies(mlibrary_static mlibrary_external)
-
-
-    set_target_properties(mlibrary_static PROPERTIES
-                          INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include/mlibrary;${CMAKE_INSTALL_PREFIX}/include"
-                          INTERFACE_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib"
-                          INTERFACE_LINK_LIBRARIES "${Geant4_LIBRARIES};Qt5::Xml;Qt5::Widgets;Qt5::Sql;cadmesh"
-                          )
-    set_property(TARGET mlibrary_static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-    set_target_properties(mlibrary_static PROPERTIES
-                          IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
-                          IMPORTED_LOCATION_RELEASE "${CMAKE_INSTALL_PREFIX}/lib/libmlibrary_static.a"
-                          )
+        add_library(mlibrary_static STATIC IMPORTED)
+        add_dependencies(dependencies mlibrary_static)
+        add_dependencies(mlibrary_static mlibrary_external)
 
 
+        set_target_properties(mlibrary_static PROPERTIES
+                              INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/include/mlibrary;${CMAKE_INSTALL_PREFIX}/include"
+                              INTERFACE_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib"
+                              INTERFACE_LINK_LIBRARIES "${Geant4_LIBRARIES};Qt5::Xml;Qt5::Widgets;Qt5::Sql;cadmesh"
+                              )
+        set_property(TARGET mlibrary_static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+        set_target_properties(mlibrary_static PROPERTIES
+                              IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+                              IMPORTED_LOCATION_RELEASE "${CMAKE_INSTALL_PREFIX}/lib/libmlibrary_static.a"
+                              )
+
+
+    endif()
 endif()
