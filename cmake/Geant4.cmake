869809e56c36d08445345068612f6fd9b59a7921
Maurik Holtrop <maurik@physics.unh.edu>
2021-04-09 12:50:29 -0400

    Add explicit build of CLHEP, since the one included with Geant4 is not fully compatible with GEMC code.

--- cmake/Geant4.cmake
+++ cmake/Geant4.cmake
@@ -11,15 +11,44 @@ if(XercesC_LIBRARY AND NOT XercesC_LIBRARY_RELEASE)            # An issue with s
 endif()
 if(NOT Geant4_FOUND)
 
+    set(CLHEP_VERSION 2.4.4.1 CACHE STRING "CLHEP version" FORCE)
     set(XercesC_VERSION 3.2.3 CACHE STRING "XercesC version" FORCE)
     set(Geant4_VERSION 10.6.3 CACHE STRING "Geant4 version" FORCE)
     set(Geant4_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/Geant4)
+
+    # The Build-in CLHEP with GEANT4 seems to not be sufficient for GEMC. (Missing RandomGaussT.h)
+    message(STATUS "Checking for CLHEP")
+    find_package(CLHEP QUIET)
+    if(NOT CLHEP_FOUND)
+        string(REPLACE "." "_" CLHEP_TAG "CLHEP_${CLHEP_VERSION}")
+        message(STATUS "***********************************************************")
+        message(STATUS "* CLHEP was not found and will be installed before GEANT4 *")
+        message(STATUS "* CLHEP version ${CLHEP_VERSION}, tag: ${CLHEP_TAG}              *")
+        message(STATUS "***********************************************************")
+
+        externalproject_add(
+                CLHEP
+                GIT_REPOSITORY https://gitlab.cern.ch/CLHEP/CLHEP.git
+                GIT_TAG   ${CLHEP_TAG}
+                SOURCE_DIR ${CMAKE_BINARY_DIR}/CLHEP
+                CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+                           -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+                           -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+                BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM}
+        )
+    else()
+        add_custom_target(CLHEP) # dummy target
+        message(STATUS "CLHEP library was found at: ${CLHEP_INCLUDE_DIRS}, ${CLHEP_LIBRARY}")
+    endif()
     #
     # The GDML component of GEANT4 depends on XercesC. Look for it, and if not found, build it.
     #
     find_package(XercesC QUIET)
     if(NOT XercesC_FOUND)
-        message(STATUS "XercesC was not found and will be installed before GEANT4")
+        message(STATUS "*************************************************************")
+        message(STATUS "* XercesC was not found and will be installed before GEANT4 *")
+        message(STATUS "* XercesC version: ${XercesC_VERSION}                                    *")
+        message(STATUS "*************************************************************")
         set(XercesC_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
         externalproject_add(
                 XercesC
@@ -28,8 +57,15 @@ if(NOT Geant4_FOUND)
                 CMAKE_ARGS       -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
                                  -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
                                  -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-                BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM} -j4
+                BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM}
         )
+        if(APPLE)
+            set(XercesC_LIBRARY ${XercesC_INSTALL_DIR}/lib/libxerces-c.dylib)
+        else()
+            set(XercesC_LIBRARY ${XercesC_INSTALL_DIR}/lib64/libxerces-c.so)
+        endif()
+        set(XercesC_INCLUDE_DIR	${XercesC_INSTALL_DIR}/include)
+
     else()
         add_custom_target(XercesC) # dummy target
         message(STATUS "XercesC library was found at: ${XercesC_LIBRARY}")
@@ -58,11 +94,12 @@ if(NOT Geant4_FOUND)
                          -DGEANT4_USE_SYSTEM_EXPAT=OFF
                          -DGEANT4_INSTALL_EXAMPLES=OFF
                          -DGEANT4_USE_QT=ON
-        BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM} -j4
+        BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM}
         UPDATE_COMMAND   ""
     )
 
     add_dependencies(Geant4 XercesC)
+    add_dependencies(Geant4 CLHEP)
 
     SET( DEPENDENCIES_RERUN_CMAKE "YES" FORCE )
 else()
