8edcd5691a024bb2c999a9dc0669627c96e530e8
Maurik Holtrop <maurik@physics.unh.edu>
2021-05-21 16:23:11 -0400

    Take care of relatively rare case when Geant4 is found but CLHEP is not.

--- cmake/Geant4.cmake
+++ cmake/Geant4.cmake
@@ -4,44 +4,51 @@
 # If GEANT4 is not found, add it to the list of packages that will be retreived and
 # build using the ExternalProject mechanism.
 #
+
+# Check for the external CLHEP.
+# The Build-in CLHEP with GEANT4 seems to not be sufficient for GEMC. (Missing RandomGaussT.h)
+message(STATUS "Checking for CLHEP")
+find_package(CLHEP QUIET)
+if(NOT CLHEP_FOUND)
+    set(CLHEP_VERSION 2.4.4.1 CACHE STRING "CLHEP version" FORCE)
+
+    string(REPLACE "." "_" CLHEP_TAG "CLHEP_${CLHEP_VERSION}")
+    message(STATUS "***********************************************************")
+    message(STATUS "* CLHEP was not found and will be installed before GEANT4 *")
+    message(STATUS "* CLHEP version ${CLHEP_VERSION}, tag: ${CLHEP_TAG}              *")
+    message(STATUS "***********************************************************")
+
+    externalproject_add(
+            CLHEP
+            GIT_REPOSITORY https://gitlab.cern.ch/CLHEP/CLHEP.git
+            GIT_TAG   ${CLHEP_TAG}
+            SOURCE_DIR ${CMAKE_BINARY_DIR}/CLHEP
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+            BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM}
+    )
+    set(CLHEP_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
+
+    SET( DEPENDENCIES_RERUN_CMAKE "YES" FORCE )
+else()
+    add_custom_target(CLHEP) # dummy target
+    message(STATUS "CLHEP library was found at: ${CLHEP_INCLUDE_DIRS}, ${CLHEP_LIBRARIES}")
+    get_filename_component(CLHEP_ROOT_DIR ${CLHEP_DIR} DIRECTORY)
+endif()
+
 message(STATUS "Checking for Geant4")
 find_package(Geant4 QUIET COMPONENTS vis_all ui_all qt gdml)
 if(XercesC_LIBRARY AND NOT XercesC_LIBRARY_RELEASE)            # An issue with some XercesC installs that do not set the _RELEAST and _DEBUG
     set(XercesC_LIBRARY_RELEASE ${XercesC_LIBRARY})
 endif()
+
 if(NOT Geant4_FOUND)
 
-    set(CLHEP_VERSION 2.4.4.1 CACHE STRING "CLHEP version" FORCE)
     set(XercesC_VERSION 3.2.3 CACHE STRING "XercesC version" FORCE)
     set(Geant4_VERSION 10.6.3 CACHE STRING "Geant4 version" FORCE)
     set(Geant4_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/Geant4)
 
-    # The Build-in CLHEP with GEANT4 seems to not be sufficient for GEMC. (Missing RandomGaussT.h)
-    message(STATUS "Checking for CLHEP")
-    find_package(CLHEP QUIET)
-    if(NOT CLHEP_FOUND)
-        string(REPLACE "." "_" CLHEP_TAG "CLHEP_${CLHEP_VERSION}")
-        message(STATUS "***********************************************************")
-        message(STATUS "* CLHEP was not found and will be installed before GEANT4 *")
-        message(STATUS "* CLHEP version ${CLHEP_VERSION}, tag: ${CLHEP_TAG}              *")
-        message(STATUS "***********************************************************")
-
-        externalproject_add(
-                CLHEP
-                GIT_REPOSITORY https://gitlab.cern.ch/CLHEP/CLHEP.git
-                GIT_TAG   ${CLHEP_TAG}
-                SOURCE_DIR ${CMAKE_BINARY_DIR}/CLHEP
-                CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-                           -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-                           -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-                BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM}
-        )
-        set(CLHEP_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
-    else()
-        add_custom_target(CLHEP) # dummy target
-        message(STATUS "CLHEP library was found at: ${CLHEP_INCLUDE_DIRS}, ${CLHEP_LIBRARIES}")
-        get_filename_component(CLHEP_ROOT_DIR ${CLHEP_DIR} DIRECTORY)
-    endif()
     #
     # The GDML component of GEANT4 depends on XercesC. Look for it, and if not found, build it.
     #
@@ -103,12 +110,12 @@ if(NOT Geant4_FOUND)
         BUILD_COMMAND    ${CMAKE_MAKE_PROGRAM}
         UPDATE_COMMAND   ""
     )
-
     add_dependencies(Geant4 XercesC)
-    add_dependencies(Geant4 CLHEP)
-
     SET( DEPENDENCIES_RERUN_CMAKE "YES" FORCE )
 else()
-    # add_custom_target(Geant4) # dummy target
+    add_custom_target(Geant4) # dummy target
     message(STATUS "Geant4 was found at: ${Geant4_DIR}")
 endif()
+
+add_dependencies(Geant4 CLHEP)
+
