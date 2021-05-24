a6fbdd6054ef18608aa68e974c6749139a002a2a
Maurik Holtrop <maurik@physics.unh.edu>
2021-05-24 10:45:39 -0400

    Clean up, and restore the BMT and FMT initialization lines.

--- cmake/CLHEP_xxxx.cmake
+++
@@ -1,8 +0,0 @@
-#
-#
-#
-message(STATUS "Checking for CLHEP")
-find_package(CLHEP)
-if(NOT CLHEP_FOUND)
-    message(ERROR "CLHEP was not found and I do not know how to build it.")
-endif()
--- cmake/SQLite3_xxxx.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS "Checking for sqlite3")
-find_package(SQLite3)
-if(NOT Qt5_FOUND)
-    message(FATAL_ERROR "********* NO System SQLite3 found, and no instructions to build it. ***********")
-
-    #
-    # TODO: Add 'get qt5' directions if qt5 is not found.
-    #
-endif()
