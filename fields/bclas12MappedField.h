73d14e50743c36dbef66b10c7b6415a519ff534c
Joseph Newton <jnewton@jlab.org>
2022-02-17 09:50:08 -0500

    Update bclas12MappedField.h

--- fields/bclas12MappedField.h
+++ fields/bclas12MappedField.h
@@ -7,10 +7,14 @@
 
 // G4 headers
 #include "G4MagneticField.hh"
+
+
+extern "C" {
 #include "magfield.h"
 #include "magfieldio.h"
 #include "munittest.h"
 #include "magfieldutil.h"
+}
 
 // c++ headers
 #include <vector>
@@ -85,9 +89,6 @@ public:
 };
 
 #endif
-
-
-
 
 
 
