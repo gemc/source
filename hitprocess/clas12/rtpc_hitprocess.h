d3cb300037c1f6aa250737e99bf36273c474e283
YuchunHung <yhung001@odu.edu>
2024-04-02 07:48:19 -0400

    Updated rtpc hitprocess and its hipo output

--- hitprocess/clas12/rtpc_hitprocess.h
+++ hitprocess/clas12/rtpc_hitprocess.h
@@ -4,6 +4,31 @@
 // gemc headers
 #include "HitProcess.h"
 
+static const double PI=3.1415926535;
+
+// constants to be used in the digitization routine
+class rtpcConstants
+{
+public:
+	
+	// database
+	int    runNo;
+	string date;
+	string connection;
+	char   database[80];
+	
+	// translation table
+	TranslationTable TT;
+	
+	// add constants here
+	  // drift parameters 
+	double z0[7], z2[7], z4[7];
+	  // gain balance (example) keep for future or reference
+	//double gain_balance[96][180]; // 180 rows, 96 cols
+	
+};
+
+
 // Class definition
 /// \class rtpc_HitProcess
 
@@ -12,6 +37,12 @@ class rtpc_HitProcess : public HitProcess
 public:
 	
 	~rtpc_HitProcess(){;}
+	
+	// constants initialized with initWithRunNumber
+	static rtpcConstants rtpcc;
+	
+	void initWithRunNumber(int runno);
+	
 	
 	// - integrateDgt: returns digitized information integrated over the hit
 	map<string, double> integrateDgt(MHit*, int);
@@ -35,10 +66,14 @@ public:
 	// - electronicNoise: returns a vector of hits generated / by electronics.
 	vector<MHit*> electronicNoise();
 	
-public:
-	// RTPC geometry parameters
-	float PAD_W, PAD_L, PAD_S, RTPC_L;
-	float phi_per_pad;
+private:
+
+	// RTPC geometry parameters (constant) These should be consistant with PadVector.java in coatjava.
+	const float PAD_W = 2.79;
+	const float PAD_L = 4.0;
+	const float PAD_S = 79.0; // old value was 80.0
+	const float RTPC_L = 384.0;
+	const float phi_per_pad = (2.0*PI)/180;
 	
 	// parameters for drift and diffustion equations for drift time, 
 	// drift angle, and diffusion in z
@@ -47,15 +82,26 @@ public:
 	float a_z, b_z;
 	
 	// variables for storing drift times and diffusion in time
-	float t_2GEM2, t_2GEM3, t_2PAD, t_2END;
-	float sigma_t_2GEM2, sigma_t_2GEM3, sigma_t_2PAD, sigma_t_gap;
+	//float sigma_t_2GEM2, sigma_t_2GEM3, sigma_t_2PAD, sigma_t_gap;
+	const float sigma_t_2GEM2 = 8.72728;
+	const float sigma_t_2GEM3 = 5.62223;
+	const float sigma_t_2PAD = 7.58056;
+	float sigma_t_gap;
 	
 	// variables for storing drift angle and diffusion in phi
-	float phi_2GEM2, phi_2GEM3, phi_2PAD, phi_2END;
-	float sigma_phi_2GEM2, sigma_phi_2GEM3, sigma_phi_2PAD, sigma_phi_gap;
+	float phi_2GEM2 = 0.0; // 0.0416925;
+	float phi_2GEM3 = 0.0; // 0.0416574;
+	float phi_2PAD = 0.0; // 0.057566;
+	float phi_2END;
+	//float sigma_phi_2GEM2, sigma_phi_2GEM3, sigma_phi_2PAD, sigma_phi_gap;
+	const float sigma_phi_2GEM2 = 0.00384579;
+	const float sigma_phi_2GEM3 = 0.00160235;
+	const float sigma_phi_2PAD = 0.00238653;
+	float sigma_phi_gap;
+
 	
 	float z_cm;
-	float TPC_TZERO;
+	float TPC_TZERO; // What's this?
 	
 	map<int, double> timeShift_map;
 	double shift_t;
